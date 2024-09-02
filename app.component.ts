33f8bc338eab129ede1aa619ee39bf2b79122aa1
Ankitadhkad <161940018+Ankitadhkad@users.noreply.github.com>
2024-09-02 11:47:36 +0530

    WebApiCoreLecture\webapicorelecture.client\src\app

---
+++ app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './shared/employee.service';
+import { Employee } from './shared/employee.model';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  employees: Employee[] = [];
+  errorMessage: string;
+
+  constructor(private employeeService: EmployeeService) { }
+
+
+
+
+    ngOnInit(): void {
+      //this.employeeService.getEmployee().subscribe(
+      //  (data: Employee[]) => {
+      //    this.employees = data;
+      //  },
+      //  (error) => {
+      //    console.error('Error fetching employee data', error);
+      //    this.errorMessage = 'There was an error fetching the employee data. Please try again later.';
+      //  }
+      //);
+    }
+    }
+
+
+
---
+++ app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeFormComponent } from './employee-details/employee-form/employee-form.component';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeDetailsComponent,
+    EmployeeFormComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule, FormsModule,
+    ToastrModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

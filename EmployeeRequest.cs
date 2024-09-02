f6d953de5b2d8f12c05981baf120b726b2b31862
Ankitadhkad <161940018+Ankitadhkad@users.noreply.github.com>
2024-09-02 11:52:34 +0530

    WebApiCoreLecture\WebApiCoreLecture.Server\ViewModels

---
+++ EmployeeRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiCoreLecture.Server.ViewModels
+{
+    public class EmployeeRequest
+
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public int Id { get; internal set; }
+    }
+}
